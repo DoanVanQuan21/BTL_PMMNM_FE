d50bff1283b1d73d37442f13da69c8c55dd2ca84
Quan <dvquan210502@gmail.com>
2023-09-30 22:35:42 +0700

    add home page

---
+++ pages/home.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'blank',
+})
+function toggleLogin() {
+  navigateTo('/login')
+}
+function toggleRegister() {
+  navigateTo('/register')
+}
+</script>
+
+<template>
+  <div class="container-lg header bg-secondary-subtle">
+    <div class="row">
+      <div class="col-lg-6 offset-lg-3 mt-2">
+        <div class="row">
+          <div class="col-lg-5 me-2 text-end">
+            <button type="submit" class="btn btn-success" @click="toggleLogin">Đăng nhập</button>
+          </div>
+          <div class="col-lg-5 ms-2 text-start">
+            <button class="btn btn-success" @click="toggleRegister">Đăng ký</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@media (max-width: 575.98px) {
+}
+@media (min-width: 576px) and (max-width: 767.98px) {
+}
+@media (min-width: 768px) and (max-width: 991.98px) {
+}
+@media (min-width: 992px) and (max-width: 1199.98px) {
+}
+@media (min-width: 1200px) and (max-width: 1399.98px) {
+}
+@media (min-width: 1400px) {
+}
+</style>
