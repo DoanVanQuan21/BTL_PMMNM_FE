2f333b392ce10d56595ec6f014780b33d9845fac
Nguyen Xuan Truong <kiaisoft.truong.nguyen@gmail.com>
2023-10-31 22:53:29 +0700

    feature login

--- constants/constants.ts
+++ constants/constants.ts
@@ -1,19 +1,30 @@
 export class TitlePage {
-  static readonly LOGIN_PAGE = 'Đăng nhập'
-  static readonly REGISTER = 'Đăng ký'
-  static readonly FORGOT_PASSWORD = 'Quên mật khẩu'
-  static readonly AUTH_ACCOUNT = 'Xác nhận mật khẩu'
-  static readonly RESET_PASSWORD = 'Đổi mật khẩu'
-  static readonly SETTING_ACCOUNT = 'Cài đặt tài khoản'
-  static readonly PATIENT_RECORDS = 'Hồ sơ bệnh nhân'
+  static readonly LOGIN_PAGE = "Đăng nhập";
+  static readonly REGISTER = "Đăng ký";
+  static readonly FORGOT_PASSWORD = "Quên mật khẩu";
+  static readonly AUTH_ACCOUNT = "Xác nhận mật khẩu";
+  static readonly RESET_PASSWORD = "Đổi mật khẩu";
+  static readonly SETTING_ACCOUNT = "Cài đặt tài khoản";
+  static readonly PATIENT_RECORDS = "Hồ sơ bệnh nhân";
 }
 export class RedirectPage {
-  static readonly HOME = '/admin/home'
-  static readonly EDIT_USER = '/admin/edit-user'
-  static readonly PATIENT_RECORDS = '/admin/patient-records'
-  static readonly LOGIN = '/auth/login'
-  static readonly RERGISTER = '/auth/register'
-  static readonly FORGOT_PASSWORD = '/auth/forgot-password'
-  static readonly AUTH_ACCOUNT = '/auth/authentication-account'
-  static readonly RESET_PASSWORD = '/auth/reset-password'
+  static readonly HOME = "/admin/home";
+  static readonly EDIT_USER = "/admin/edit-user";
+  static readonly PATIENT_RECORDS = "/admin/patient-records";
+  static readonly LOGIN = "/auth/login";
+  static readonly RERGISTER = "/auth/register";
+  static readonly FORGOT_PASSWORD = "/auth/forgot-password";
+  static readonly AUTH_ACCOUNT = "/auth/authentication-account";
+  static readonly RESET_PASSWORD = "/auth/reset-password";
+}
+
+export class ResponseStatus {
+  static readonly HTTP_OK = 200;
+  static readonly HTTP_UNAUTHORIZED = 401;
+  static readonly HTTP_PAYMENT_REQUIRED = 402;
+  static readonly HTTP_FORBIDDEN = 403;
+  static readonly HTTP_NOT_FOUND = 404;
+  static readonly HTTP_UNPROCESSABLE_ENTITY = 422;
+  static readonly HTTP_INTERNAL_SERVER_ERROR = 500;
+  static readonly HTTP_BAD_GATEWAY = 502;
 }
---
+++ plugins/toastify.client.ts
@@ -0,0 +1,46 @@
+import Vue3Toasity, { type ToastContainerOptions } from "vue3-toastify";
+import "vue3-toastify/dist/index.css";
+import { toast, type ToastOptions } from "vue3-toastify";
+import "@/assets/scss/main.scss";
+
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.vueApp.use(Vue3Toasity) as ToastContainerOptions;
+  return {
+    provide: {
+      toastify: {
+        delay: 0,
+        position: toast.POSITION.TOP_RIGHT,
+        clearOnUrlChange: true,
+        bodyClassName: "body-toastify",
+        success: (msg: string) => {
+          toast(msg, {
+            limit: 5,
+            type: "success",
+            autoClose: 1000,
+          } as ToastOptions);
+        },
+        error: (msg: string) => {
+          toast(msg, {
+            limit: 5,
+            type: "error",
+            autoClose: 2000,
+          } as ToastOptions);
+        },
+        info: (msg: string) => {
+          toast(msg, {
+            limit: 5,
+            type: "info",
+            autoClose: 2000,
+          } as ToastOptions);
+        },
+        warning: (msg: string) => {
+          toast(msg, {
+            limit: 5,
+            type: "warning",
+            autoClose: 2000,
+          } as ToastOptions);
+        },
+      },
+    },
+  };
+});
